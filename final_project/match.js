f67ce7b2fe53b1808cfaaa55e568a4795986ae99
Mai Li <sillyboylm@hotmail.com>
2018-11-29 22:27:29 -0700

    test_blog

---
+++ final_project/match.js
@@ -0,0 +1,10 @@
+function match(){
+
+    document.getElementById("right").innerHTML='';
+    var iframe=document.createElement('iframe');
+    iframe.setAttribute('src','match.html');
+    iframe.setAttribute('width','78%');
+    iframe.setAttribute('height','680px');
+    iframe.setAttribute('float','left');
+    document.getElementById("right").appendChild(iframe);
+}
